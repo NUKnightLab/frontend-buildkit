d87c62c87cb7d875665712e4142b3f015fdf4294
Divya Sasidharan <hello@shortdiv.com>
2016-07-13 22:07:54 -0500

    fix: Adding hbs helper

--- tasks/compile-hbs.js
+++ tasks/compile-hbs.js
@@ -3,7 +3,11 @@ var _ = require('lodash'),
     fs = require('fs-extra'),
     path = require('path'),
     Handlebars = require('handlebars'),
-    globby = require('globby');
+    globby = require('globby'),
+    helper = require('./helpers/partial-builder.js');
+
+//run Helpers
+helper.getPartial(Handlebars, 'src/templates/partials');
 
 function renderTemplate(templatePath) {
   var file = fs.readFileSync(templatePath, 'utf8'),
@@ -29,9 +33,9 @@ function build() {
     var filePattern = path.dirname(file).split('src/templates/')[1],
         fileName = path.basename(file, '.hbs'),
         template = renderTemplate(file),
-        page = renderPage(template, 'layouts/default.hbs');
+        page = renderPage(template, 'src/templates/layouts/default.hbs');
 
-    if(!!filePattern.length) {
+    if(!!filePattern) {
       fs.outputFileSync(`dist/templates/${filePattern}/${fileName}.html`)
     } else {
       fs.outputFileSync(`dist/templates/${fileName}.html`, page, 'utf8');
