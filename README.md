658038f0aad772595bcf899556610c82478d9228
Zach Wise <zach@digitalartwork.net>
2018-07-09 09:30:13 -0500

    Update to latest frontendkit being used in practice in several projects

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Frontend BuildKit
-This is the basic starter kit for frontend projects
+This is the basic starter kit for Knight Lab frontend projects
 
 ## Installation
 
@@ -7,8 +7,9 @@ To use this template, your computer needs:
 
 - [NodeJS](https://nodejs.org/en/) (0.12 or greater)
 
+
 ## Using the BuildKit
-Install the BuildKit by running this command from the project directory:
+Install the BuildKit dependencies by running this command from the project directory:
 ```bash
 npm install
 ```
@@ -16,4 +17,8 @@ npm install
 Use this command to run the auto-compiler:
 ```bash
 npm run start
+```
+Use this command to compile for distribution:
+```bash
+npm run dist
 ```
--- webpack.config.js
+++ webpack.config.js
@@ -4,18 +4,48 @@ var webpack = require('webpack'),
     componentPath = path.resolve('./src/js');
 
 module.exports = {
-  context: path.join(__dirname), 
-  entry: [
-    "./src/js/app.js"
-  ],
-  output: {
-    path: path.join(__dirname, "./dist/js"),
-    filename: "[name].js"
-  },
-  resolve: {
-    root: componentPath
-  },
-  resolveLoader: {
-    root: path.join(__dirname, "node_modules")
-  }
+    context: path.join(__dirname),
+    entry: ["./src/js/app.js"],
+    output: {
+        path: path.join(__dirname, "./dist/js"),
+        filename: "[name].js"
+    },
+    resolve: {
+        modules: [path.resolve('./src/js'), "node_modules"]
+    },
+    module: {
+        rules: [
+            {
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['babel-preset-env']
+                    }
+                }
+            }
+        ]
+    ,
+    optimization: {
+        minimize:true,
+        minimizer: [
+            new UglifyJsPlugin({
+                sourceMap: true,
+                uglifyOptions: {
+                    ecma: 6,
+                    warnings: false,
+                    compress: {
+                        unused: true,
+                        dead_code: true
+                    },
+                    ie8: false,
+                    keep_classnames: false,
+                    output: {
+                        beautify: false,
+                        comments: false
+                    }
+                }
+            })
+        ]
+    }
 }
