8383c1d37c1ab5184cb33794d73c90c3aa71e4c6
Zach Wise <zach@digitalartwork.net>
2019-01-07 10:55:34 -0600

    More useful JS starting point

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,4 @@
-var first = require('first.js');
-var second = require('second.js');
 
+const version = 'Frontend BuildKit Version: 2.0.0 (2019-01-04)';
+const EventEmitter = require("./utils/EventEmitter.js");
+const dom = require("./utils/dom.js");
